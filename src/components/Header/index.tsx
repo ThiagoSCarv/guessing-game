e1576750f920c1deb3f8200bc02496f18f274a76
Thiago Soares <thiagocarv4@gmail.com>
2025-05-08 19:09:40 -0300

    finalizando header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,23 @@
 import logo from "../../assets/logo.png";
 
+import restart from "../../assets/restart.svg";
+
 import style from "./style.module.css";
 
 export function Header() {
   return (
     <div className={style.container}>
       <img src={logo} alt="Logo" />
+
+      <header>
+        <span>
+          <strong>5</strong> de 10 tentativas
+        </span>
+
+        <button type="button">
+          <img src={restart} alt="Ícone de Reiniciar" />
+        </button>
+      </header>
     </div>
   );
 }
