3a2bb2f6a18c628764ee4e1b21d5ca8362a58dde
Thiago Soares <thiagocarv4@gmail.com>
2025-05-09 18:23:54 -0300

    adicionando cores em wrong e correct

--- src/components/Letter/index.tsx
+++ src/components/Letter/index.tsx
@@ -1,12 +1,24 @@
-import style from "./style.module.css"
+import style from "./style.module.css";
 
 type Props = {
   value?: string;
+  size?: "default" | "small";
+  color?: "default" | "correct" | "wrong";
 };
 
-export function Letter({ value }: Props) {
+export function Letter({
+  value = "",
+  size = "default",
+  color = "default",
+}: Props) {
   return (
-    <div className={style.letter}>
+    <div
+      className={`
+        ${style.letter} 
+        ${size === "small" && style.letterSmall} 
+        ${color === "correct" && style.letterCorrect} 
+        ${color === "wrong" && style.letterWrong}`}
+    >
       <span>{value}</span>
     </div>
   );
--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,7 +1,16 @@
 import styles from "./style.module.css";
 
+import { Letter } from "../Letter";
+
 export function LettersUsed() {
-  return <div className={styles.lettersUsed}>
-    <h5>Letras utilizadas</h5>
-  </div>;
+  return (
+    <div className={styles.lettersUsed}>
+      <h5>Letras utilizadas</h5>
+
+      <div>
+        <Letter value="X" size="small" color="correct" />
+        <Letter value="R" size="small" color="wrong" />
+      </div>
+    </div>
+  );
 }
